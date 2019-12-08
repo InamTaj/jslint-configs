ca4ea2e2c641d90a180bc9a7f0d693740912a411
Inam Taj <inamullahtaj@gmail.com>
2019-12-08 14:40:26 +0500

    :recycle: fixes arrow-body-style rule for JS

--- config-base.js
+++ config-base.js
@@ -195,9 +195,7 @@ module.exports = {
      * ES6 Rules
      */
     'arrow-body-style': [2,          // https://eslint.org/docs/rules/arrow-body-style
-      'as-needed', {
-        'requireReturnForObjectLiteral': true,
-      },
+      'as-needed',
     ],
     'arrow-parens': [2,              // https://eslint.org/docs/rules/arrow-parens
       'as-needed',
